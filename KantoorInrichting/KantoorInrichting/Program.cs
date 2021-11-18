e6afb86137417f186b579fcced46294572604646
GewoonRox <74775299+GewoonRox@users.noreply.github.com>
2021-11-18 11:02:34 +0100

    test1

---
+++ KantoorInrichting/KantoorInrichting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KantoorInrichting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
