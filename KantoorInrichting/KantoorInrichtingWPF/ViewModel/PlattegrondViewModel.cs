4f128f5737a38c097791dc97e601cf3b83b4ea42
GewoonRox <74775299+GewoonRox@users.noreply.github.com>
2021-12-10 15:50:30 +0100

    afbeeldingen aangepast

---
+++ KantoorInrichting/KantoorInrichtingWPF/ViewModel/PlattegrondViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace KantoorInrichtingWPF.ViewModel
+{
+    class PlattegrondViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        public PlattegrondViewModel() 
+        {
+
+        }
+        private void OnPropertyChangedEvent(string propertyName)
+        {
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        }
+    }
+}
