b212617fe401d79b312347d2f0dabdbecfe68235
smellody15 <61581896+Muisschuiver26@users.noreply.github.com>
2021-12-23 11:32:02 +0100

    afadsfdsa
    code overgezet

--- KantoorInrichting/KantoorInrichtingWPF/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ KantoorInrichting/KantoorInrichtingWPF/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -127,7 +127,7 @@ namespace KantoorInrichtingWPF {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/KantoorInrichtingWPF;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/KantoorInrichtingWPF;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
