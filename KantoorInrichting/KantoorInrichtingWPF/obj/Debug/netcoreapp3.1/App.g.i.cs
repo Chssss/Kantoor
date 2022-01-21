95ee5ab7e0e3dce2006188d9225d1f58bf48bd82
GewoonRox <74775299+GewoonRox@users.noreply.github.com>
2022-01-21 11:10:08 +0100

    new releas executeble gemaakt

--- KantoorInrichting/KantoorInrichtingWPF/obj/Debug/netcoreapp3.1/App.g.i.cs
+++ KantoorInrichting/KantoorInrichtingWPF/obj/Debug/netcoreapp3.1/App.g.i.cs
@@ -45,7 +45,7 @@ namespace KantoorInrichtingWPF {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.13.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.9.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace KantoorInrichtingWPF {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.13.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.9.0")]
         public static void Main() {
             KantoorInrichtingWPF.App app = new KantoorInrichtingWPF.App();
             app.InitializeComponent();
