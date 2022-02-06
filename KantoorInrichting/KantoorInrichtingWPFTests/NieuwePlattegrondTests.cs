ad83fa974362199d6a19e595a63071f411dfc86a
Benjamin Booy <70954419+BenjaminBooy@users.noreply.github.com>
2022-02-06 23:58:38 +0100

    Een paar unit tests toegevoegd

---
+++ KantoorInrichting/KantoorInrichtingWPFTests/NieuwePlattegrondTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KantoorInrichtingWPF;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KantoorInrichtingWPF.Tests
+{
+    [TestClass()]
+    public class NieuwePlattegrondTests
+    {
+        [TestMethod()]
+        public void NieuwePlattegrondTest()
+        {
+            // Test het geval waarin tekst word doorgegeven in plaats van een decimaal voor de hoogte
+            string TBHoogte = "Testcode";
+            Assert.IsInstanceOfType(TBHoogte, typeof(decimal));
+        }
+    }
+}
---
+++ KantoorInrichting/KantoorInrichtingWPFTests/ViewModel/MeubelViewModelTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KantoorInrichtingWPF.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KantoorInrichtingWPF.ViewModel.Tests
+{
+    [TestClass()]
+    public class MeubelViewModelTests
+    {
+        [TestMethod()]
+        public void MeubelViewModelTest()
+        {         
+            // Test het geval waarin tekst word doorgegeven in plaats van een getal voor een prijs
+            string _prijs = "Voetbal";
+            Assert.IsInstanceOfType(_prijs, typeof(int));
+        }
+
+        [TestMethod()]
+        public void StaticUpdateCatalogusTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateCatalogusExecuteTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void XmlInvoegenTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
